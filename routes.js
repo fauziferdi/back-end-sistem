8ac396e0aba73217c909560f64af8fb4825a29de
EC2 Default User <ec2-user@ip-172-31-35-182.ap-southeast-2.compute.internal>
2023-06-02 00:55:42 +0000

    1.Unggah Pertama

---
+++ routes.js
@@ -0,0 +1,32 @@
+
+const routes = [
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },{
+        method: 'GET',
+        path: '/test',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+];
+
+module.exports = routes;
